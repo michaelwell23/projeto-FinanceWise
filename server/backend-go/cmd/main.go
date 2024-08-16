c2710629a7c12572ff6a19611935eb08d345da43
Michael Wellington Lopes <michael23.wellington@gmail.com>
2024-08-16 19:06:57 -0300

    Creating struture backend golang

---
+++ server/backend-go/cmd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Bem-vindo ao servidor Go!")
+	})
+	port := ":8080"
+
+	log.Printf("Servidor rodando na porta %s...", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatalf("Erro ao iniciar o servidor: %v", err)
+	}
+}
---
+++ server/backend-go/go.mod
@@ -0,0 +1,3 @@
+module github.com/michaelwell23/backend-go
+
+go 1.23.0
