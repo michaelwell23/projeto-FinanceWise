96e2ea6975a445568acc82ce4833e3f883d5c015
Michael Wellington Lopes <michael23.wellington@gmail.com>
2024-08-24 08:30:00 -0300

    Starting project over: Creating connection to database using docker-compose, postgres and typeorm

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,29 +1,9 @@
-import { validateUser } from './middlewares/validateUser';
 import { Router } from 'express';
-import multer from 'multer';
-
-import { UsersController } from './controllers/UsersController';
-
-import multerConfig from './config/multer';
-import { authMiddleware } from './middlewares/authmiddleware';
 
 const router = Router();
-const upload = multer(multerConfig);
 
-const usersController = new UsersController();
-
-router.post(
-  '/users',
-  upload.single('avatar'),
-  validateUser,
-  usersController.createUsers
-);
-
-router.use(authMiddleware);
-
-router.get('/users', usersController.getAllUsers);
-router.get('/users/:id', usersController.getUserById);
-router.put('/users/:id', upload.single('avatar'), usersController.upadate);
-router.delete('/users/:id', usersController.delete);
+router.get('/', (req, res) => {
+  res.json({ message: 'Hello World' });
+});
 
 export default router;
