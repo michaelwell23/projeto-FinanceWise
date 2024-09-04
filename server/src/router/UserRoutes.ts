30287747786a703e84391db4d436991e9d60e777
Michael Wellington Lopes <michael23.wellington@gmail.com>
2024-09-04 07:57:08 -0300

    Starting project over: Refactoring user routes

---
+++ server/src/router/UserRoutes.ts
@@ -0,0 +1,31 @@
+import multer from 'multer';
+import { Router } from 'express';
+
+const userRouter = Router();
+
+import multerConfig from '../config/multer';
+const upload = multer(multerConfig);
+
+import { UserCreateController } from '../controllers/UserControllers/UserCreate';
+import { UserDeleteController } from '../controllers/UserControllers/UserDelete';
+import { UserGetAllController } from '../controllers/UserControllers/UserGetAll';
+import { UserGetOneController } from '../controllers/UserControllers/UserGetOne';
+import { UserUpdateController } from '../controllers/UserControllers/UserUpdate';
+
+const userCreateController = new UserCreateController();
+const userGetAllController = new UserGetAllController();
+const userGetOneController = new UserGetOneController();
+const userUpdateController = new UserUpdateController();
+const userDeleteController = new UserDeleteController();
+
+userRouter.post('/users', userCreateController.create);
+userRouter.get('/users', userGetAllController.list);
+userRouter.get('/users/:id', userGetOneController.show);
+userRouter.put(
+  '/users/:id',
+  upload.single('avatar'),
+  userUpdateController.update
+);
+userRouter.delete('/users/:id', userDeleteController.delete);
+
+export default userRouter;
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,27 +1,8 @@
-import multer from 'multer';
 import { Router } from 'express';
+import userRouter from './router/UserRoutes';
 
-import multerConfig from './config/multer';
-const router = Router();
+const routes = Router();
 
-const upload = multer(multerConfig);
+routes.use(userRouter);
 
-import { UserCreateController } from './controllers/UserControllers/UserCreate';
-import { UserDeleteController } from './controllers/UserControllers/UserDelete';
-import { UserGetAllController } from './controllers/UserControllers/UserGetAll';
-import { UserGetOneController } from './controllers/UserControllers/UserGetOne';
-import { UserUpdateController } from './controllers/UserControllers/UserUpdate';
-
-const userCreateController = new UserCreateController();
-const userGetAllController = new UserGetAllController();
-const userGetOneController = new UserGetOneController();
-const userUpdateController = new UserUpdateController();
-const userDeleteController = new UserDeleteController();
-
-router.post('/users', userCreateController.create);
-router.get('/users', userGetAllController.list);
-router.get('/users/:id', userGetOneController.show);
-router.put('/users/:id', upload.single('avatar'), userUpdateController.update);
-router.delete('/users/:id', userDeleteController.delete);
-
-export default router;
+export default routes;
