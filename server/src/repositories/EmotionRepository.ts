3332d9f65215edcac160d4fd2aef41f23c4b9734
Michael Wellington Lopes <michael23.wellington@gmail.com>
2024-10-21 14:00:00 -0300

    Iniciando novo projeto

--- server/src/repositories/EmotionRepository.ts
+++
@@ -1,5 +0,0 @@
-import { Emotion } from './../entities/Emotion';
-import { EntityRepository, Repository } from 'typeorm';
-
-@EntityRepository(Emotion)
-export class EmotionRepository extends Repository<Emotion> {}
--- server/src/repositories/SuggestionRepository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { Suggestion } from '../entities/Suggestion';
-
-@EntityRepository(Suggestion)
-export class SuggestionRepository extends Repository<Suggestion> {}
