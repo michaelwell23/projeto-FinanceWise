6ed0914042d635214118da67fc771fa7a46598c6
Michael Wellington Lopes <michael23.wellington@gmail.com>
2024-12-02 07:41:22 -0300

    05 - testing budget routes (GetCreate)

--- server/src/controllers/Budget/BudgetCreateController.ts
+++ server/src/controllers/Budget/BudgetCreateController.ts
@@ -2,19 +2,18 @@ import { Request, Response } from 'express';
 import { BudgetCreateService } from '../../services/Budget/BudgetCreateService';
 
 export class BudgetCreateController {
-  private budgetCreateService: BudgetCreateService;
-
-  constructor() {
-    this.budgetCreateService = new BudgetCreateService();
-  }
-
   async create(request: Request, response: Response): Promise<Response> {
-    const { userId } = request;
     const { maxAmount } = request.body;
 
+    if (!request.userId) {
+      return response.status(401).json({ error: 'User not authenticated' });
+    }
+
+    const budgetCreateService = new BudgetCreateService();
+
     try {
-      const budget = await this.budgetCreateService.createBudget({
-        userId,
+      const budget = await budgetCreateService.createBudget({
+        userId: request.userId,
         maxAmount,
       });
 
