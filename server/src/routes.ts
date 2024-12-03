7143fb4c038a3638a0e0989369a0576606002279
Michael Wellington Lopes <michael23.wellington@gmail.com>
2024-12-03 19:59:33 -0300

    05 - deleting expense entity

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,7 +4,6 @@ import userRoutes from './router/userRouter';
 import accountRoutes from './router/accountRouter';
 import loginRoutes from './router/loginRouter';
 import budgetRoutes from './router/budgetRouter';
-import expenseRouter from './router/expenceRouter';
 
 const routes = Router();
 
@@ -12,7 +11,6 @@ routes.use(userRoutes);
 routes.use(loginRoutes);
 
 routes.use(accountRoutes);
-routes.use(expenseRouter);
 routes.use(budgetRoutes);
 
 export default routes;
